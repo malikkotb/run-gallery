70e059211ae86390264f434979738090017fea5d
malik <malikkotb@icloud.com>
2024-06-19 10:56:59 +0200

    smooth scroll

--- app/page.js
+++ app/page.js
@@ -1,5 +1,20 @@
-import Gallery from "../components/gallery";
+"use client";
+import { useEffect } from "react";
+import Lenis from "lenis";
+import Gallery from "@/components/gallery";
+
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main>
       <section className="h-screen flex justify-center items-center">
